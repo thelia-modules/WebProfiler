63d6896c32579668d11132e459d6ef87243ae28d
Alexandre Nozière <anoziere@openstudio.fr>
2026-04-12 13:42:12 +0200

    fix: update Thelia class reference after Domain refactoring
    Thelia\Core\Thelia was renamed to Thelia\Core\TheliaKernel in
    thelia/thelia commit 5f9eed85d (Feat: Domain #3346).
    Also add explicit nullable type on $exception parameter.

--- DataCollector/TheliaCollector.php
+++ DataCollector/TheliaCollector.php
@@ -15,14 +15,14 @@ namespace WebProfiler\DataCollector;
 use Symfony\Bundle\FrameworkBundle\DataCollector\AbstractDataCollector;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Thelia\Core\Thelia;
+use Thelia\Core\TheliaKernel;
 
 class TheliaCollector extends AbstractDataCollector
 {
-    public function collect(Request $request, Response $response, \Throwable $exception = null): void
+    public function collect(Request $request, Response $response, ?\Throwable $exception = null): void
     {
         $this->data = [
-            'theliaVersion' => Thelia::THELIA_VERSION,
+            'theliaVersion' => TheliaKernel::THELIA_VERSION,
         ];
     }
 
